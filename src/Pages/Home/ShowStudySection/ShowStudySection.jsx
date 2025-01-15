ab9e2447190e6e8d69a811f6c4f5ad27ee177a55
shahidul shohid <shahidulislamshohid7gmail.com>
2025-01-15 22:55:23 +0600

    show study session section implement is done

--- src/Pages/Home/ShowStudySection/ShowStudySection.jsx
+++ src/Pages/Home/ShowStudySection/ShowStudySection.jsx
@@ -1,22 +1,65 @@
-import {useQuery} from '@tanstack/react-query'
-import useAxiosPublic from '../../../Hooks/useAxiosPublic';
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../../Hooks/useAxiosPublic";
+import { compareAsc, format } from "date-fns";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-router-dom";
 const ShowStudySection = () => {
-    const axiosPublic = useAxiosPublic()
-    const {data: session = [], isLoading, refetch} = useQuery({
-        queryKey: ['session'],
-        queryFn: async() => {
-           const res = await axiosPublic.get('/studySessions')
-           return res.data
-        }
-    })
-    console.log(session)
-    return (
-        <div>
-           {
-            session?.map(item => <div key={item._id}></div>)
-           }
+  const axiosPublic = useAxiosPublic();
+  const {
+    data: session = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["session"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/studySessions");
+      return res.data;
+    },
+  });
+
+  // for showing closed or ongoing
+  const currentDate = new Date().toLocaleString("en-us");
+  console.log(currentDate);
+  console.log(session);
+  return (
+    <div className="my-12">
+        <h3 className="text-center text-2xl md:text-3xl font-semibold my-5">Show Study Session</h3>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+      {session?.map((item) => (
+        <div key={item._id}>
+          <div>
+            <img
+              className="h-[250px] w-full object-cover"
+              src={item.image}
+              alt=""
+            />
+            <div className="p-3 border space-y-1">
+              <h3 className="text-xl font-semibold">{item.title}</h3>
+              <p className="text-gray-500">{item.description}</p>
+              <div className="flex items-center gap-1">
+                <p className="text-xl">Registration is</p>
+                <p>
+                  <HiOutlineArrowNarrowRight className="w-8 mt-1" />
+                </p>
+                <p>
+                  {compareAsc(
+                    new Date(),
+                    format(new Date(item.resEndDate), "P")
+                  ) === 1 ? (
+                    <button className="text-lg text-red-500">Closed</button>
+                  ) : (
+                    <button className="text-lg text-green-500">Ongoing</button>
+                  )}
+                </p>
+              </div>
+              <Link><button className="px-5 py-1 bg-blue-500 text-white rounded-sm text-lg mt-3">Read More</button></Link>
+            </div>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+    </div>
+  );
 };
 
 export default ShowStudySection;
