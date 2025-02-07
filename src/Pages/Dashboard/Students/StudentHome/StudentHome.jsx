e4e93522a31f9fd9ee1532e12dbc2c4e4ac61d01
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-02-07 22:43:45 +0600

    student dashboard is some update

--- src/Pages/Dashboard/Students/StudentHome/StudentHome.jsx
+++ src/Pages/Dashboard/Students/StudentHome/StudentHome.jsx
@@ -2,17 +2,23 @@ import useAdmin from "../../../../Hooks/useAdmin";
 import useAuth from "../../../../Hooks/useAuth";
 
 const StudentHome = () => {
-    const [role] = useAdmin()
-    const {user} = useAuth()
-    console.log(role)
-    return (
-        <div>
-            <h3 className="text-center text-gray-600 text-xl md:text-3xl lg:text-4xl font-bold mt-12 mb-5 uppercase">Welcome to <strong className="text-blue-500">{role}</strong> Dashboard</h3>
-            <div>
-                <img className="w-2/4 h-[200px] object-cover mx-auto rounded-xl" src={user?.photoURL} alt="" />
-            </div>
-        </div>
-    );
+  const [role] = useAdmin();
+  const { user } = useAuth();
+  console.log(role);
+  return (
+    <div>
+      <h2 className="text-center text-2xl md:text-3xl font-bold text-green-500 mb-6">
+        Hi, Welcome {user.displayName}
+      </h2>
+      <div>
+        <img
+          className="w-2/4 h-[200px] object-cover mx-auto rounded-xl"
+          src={user?.photoURL}
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default StudentHome;
