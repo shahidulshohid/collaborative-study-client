cf460afbf0e672818cd8b5841f1e6e93c4f06a44
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-02-18 08:36:22 +0600

    dynamic titlt create is done

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,13 @@
+import { useEffect } from "react";
 import Container from "../../Shared/Container/Container";
 import Banner from "../Banner/Banner";
 import ShowStudySection from "../ShowStudySection/ShowStudySection";
 import TutorSection from "./TutorSection/TutorSection";
 
 const Home = () => {
+  useEffect(() => {
+    window.document.title = "Home" || "StudySphere"
+  }, [])
   return (
     <div>
       <Banner></Banner>
