fca9fdd2acee1586c11483182aa1b1b81d7dd562
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-04-29 10:08:49 +0600

    update metarials is update

--- src/Pages/Dashboard/Tutor/UpdateMaterialTutor/UpdateMaterialTutor.jsx
+++ src/Pages/Dashboard/Tutor/UpdateMaterialTutor/UpdateMaterialTutor.jsx
@@ -59,7 +59,7 @@ const UpdateMaterialTutor = () => {
       <h3 className="text-center mb-5 text-2xl md:text-3xl font-semibold">
         Update Materials
       </h3>
-      <div className="bg-[#d0e293] md:w-3/4 mx-auto px-5 lg:px-8 py-6 rounded-xl">
+      <div className="md:w-3/4 mx-auto px-5 lg:px-8 py-6 rounded-xl">
         <form onSubmit={handleSubmit(onSubmit)}>
           <label className="form-control w-full">
             <div className="label">
